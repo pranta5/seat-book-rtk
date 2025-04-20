3cb8f72601c80cfd4ee09f2e0b5ab0bbcaab4d5c
pranta5 <prantadas01@gmail.com>
2025-04-20 19:45:47 +0530

    dependency

--- src/components/SeatManager.tsx
+++ src/components/SeatManager.tsx
@@ -1,44 +1,45 @@
-'use client'
-import { getRoomByIdThunk } from '@/features/rooms/roomThunks'
-import { useAppDispatch, useAppSelector } from '@/store/hooks'
-import { Seat } from '@/types/rooms'
-import React, { useEffect, useState } from 'react'
-import SeatGrid from './SeatGrid'
+"use client";
+import { getRoomByIdThunk } from "@/features/rooms/roomThunks";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { Seat } from "@/types/rooms";
+import React, { useEffect, useState } from "react";
+import SeatGrid from "./SeatGrid";
 
-interface seatprops{
-    roomId:string
+interface seatprops {
+  roomId: string;
 }
 
-const SeatManager = ({roomId}:seatprops) => {
-  const {isAdmin} = useAppSelector(state=>state.auth)
-  const [seats,setSeats]=useState<Seat[][]>([])
-  const [roomName,setRoomName] = useState("")
+const SeatManager = ({ roomId }: seatprops) => {
+  const { isAdmin } = useAppSelector((state) => state.auth);
+  const [seats, setSeats] = useState<Seat[][]>([]);
+  const [roomName, setRoomName] = useState("");
 
-    const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
-    const fetchRoom = async()=>{
-        const room = await dispatch(getRoomByIdThunk(roomId)) as { payload: { name: string; layout: Seat[][] } }
-        // console.log('room',room);
-        setRoomName(room.payload?.name)
-        setSeats(room?.payload?.layout)
-    }
-    // console.log("seats",seats);
-    
+  // console.log("seats",seats);
 
-    useEffect(()=>{
-        fetchRoom()
-    },[roomId])
+  useEffect(() => {
+    const fetchRoom = async () => {
+      const room = (await dispatch(getRoomByIdThunk(roomId))) as {
+        payload: { name: string; layout: Seat[][] };
+      };
+      // console.log('room',room);
+      setRoomName(room.payload?.name);
+      setSeats(room?.payload?.layout);
+    };
+    fetchRoom();
+  }, [roomId, dispatch]);
 
   return (
-    <div className='p-6 mt-10'>
-      <h2 className='text-center my-4'>{roomName}</h2>
+    <div className="p-6 mt-10">
+      <h2 className="text-center my-4">{roomName}</h2>
       {seats.length > 0 && (
         <>
-        <SeatGrid seats={seats} isAdmin={isAdmin} roomId={roomId}/>
+          <SeatGrid seats={seats} isAdmin={isAdmin} roomId={roomId} />
         </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default SeatManager
+export default SeatManager;
